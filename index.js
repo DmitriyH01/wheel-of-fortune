cc30d2e0bad4953cd890c901f9b56c65c075a93b
DIMA <t0perxxl@gmail.com>
2021-10-08 18:31:49 +0300

    add wheel & wheel style

--- index.js
+++ index.js
@@ -11,14 +11,36 @@ function createMainContent() {
 
   fragment.appendChild(main);
 
-  main.innerHTML = createTable();
+  main.insertAdjacentHTML("beforeend", `${createTable()}`);
+  main.insertAdjacentHTML("beforeend", `${createWheel()}`);
   BODY.appendChild(fragment);
 }
 
 function createTable() {
   return `
-   <section className="table">
-     <span className="table__numbers"></span>
-     <ul className="table__figures"></ul>
+   <section class="table">
+     <span class="table__numbers"></span>
+     <ul class="table__figures"></ul>
    </section>`;
+}
+
+function createWheel() {
+  return `<section class="wheel_area">
+      <span class="wheel_area__text">testwork</span>
+      <div class="wheel_area__wheel">
+        <div class="wheel_area__wheel__inner">
+          <ul class="wheel_area__wheel__inner__list">
+          </ul>
+        </div>
+        <div class="wheel_area__wheel__rectangle_left"></div>
+        <div class="wheel_area__wheel__rectangle_right"></div>
+      </div>
+      <div class="wheel_area__buttons_wrap">
+        <button class="wheel_area__buttons_wrap__start">start</button>
+        <button
+          class="wheel_area__buttons_wrap__stop">
+          stop
+        </button>
+      </div>
+    </section>`;
 }
