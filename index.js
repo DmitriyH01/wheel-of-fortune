54be4a2ab75bbe5d13cedd355c088aacca7f0c13
DIMA <t0perxxl@gmail.com>
2021-10-08 17:51:45 +0300

    add table

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import { figures } from "./modules/storage.js";
 
+const BODY = document.querySelector("body");
 
+createMainContent();
+
+function createMainContent() {
+  let fragment = document.createDocumentFragment();
+  const main = document.createElement("main");
+  main.className = "main";
+
+  fragment.appendChild(main);
+
+  main.innerHTML = createTable();
+  BODY.appendChild(fragment);
+}
+
+function createTable() {
+  return `
+   <section className="table">
+     <span className="table__numbers"></span>
+     <ul className="table__figures"></ul>
+   </section>`;
+}
