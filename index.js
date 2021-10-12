645a4194d757debded7a409b746c45a14c650978
DIMA <t0perxxl@gmail.com>
2021-10-12 14:01:04 +0300

    timeline method = false

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { figures } from "./modules/storage.js";
-gsap.registerPlugin(ScrollTrigger);
-var tl = gsap.timeline({ repeat: -1, delay: 0 });
+// gsap.registerPlugin(ScrollTrigger);
 
 const WHEEL = document.querySelector(".wheel_area__wheel__inner__list");
 const TABLE = document.querySelector(".table__figures");
-let ifFirstPress = true;
-// let count = 0;
+let tl = gsap.timeline({ repeat: -1, delay: 0 }).add("same");
+
+let ifStartPress = true;
+let count = 0;
 
 fillWheelContent(figures);
 fillTableContent(figures);
@@ -13,34 +14,51 @@ fillTableContent(figures);
 document
   .querySelector(".wheel_area__buttons_wrap__start")
   .addEventListener("click", () => {
+    ifStartPress = true;
+
+    fillWheelContent(figures);
     startWheel();
   });
 document
   .querySelector(".wheel_area__buttons_wrap__stop")
   .addEventListener("click", () => {
     tl.duration(8);
-    setTimeout(() => tl.pause(), 2500);
+    setTimeout(() => tl.pause(0), 2500);
+    setTimeout(() => fillWheelContent(figures), 2500);
   });
 
-function fillWheelContent(arr) {
+function fillWheelContent(figures) {
+  console.log(ifStartPress);
   let fragment = document.createDocumentFragment();
+  WHEEL.innerHTML = "";
   fragment.appendChild(WHEEL);
 
-  // let maxThreeArr = [count, count + 1, count + 2];
-
-  // if (count === arr.length - 1) {
-  //   maxThreeArr = [count, 0, 1];
-  // }
-  // if (count === arr.length - 2) {
-  //   maxThreeArr = [count, count + 1, 0];
-  // }
-
-  arr.forEach((el, index) => {
-    let template = ` <li id = "item${index}" class="wheel_area__wheel__inner__list__item">
+  if (ifStartPress) {
+    figures.forEach((el, index) => {
+      let template = ` <li id = "item${index}" class="wheel_area__wheel__inner__list__item">
           <img src=${el}></img>
         </li>`;
-    WHEEL.insertAdjacentHTML("beforeEnd", template);
-  });
+      WHEEL.insertAdjacentHTML("beforeEnd", template);
+    });
+  }
+  if (!ifStartPress) {
+    count = randomInteger(0, 39);
+    let maxThreeArr = [count, count + 1, count + 2];
+
+    if (count === figures.length - 1) {
+      maxThreeArr = [count, 0, 1];
+    }
+    if (count === figures.length - 2) {
+      maxThreeArr = [count, count + 1, 0];
+    }
+
+    maxThreeArr.forEach((el, index) => {
+      let template = ` <li id = "item${index}" class="wheel_area__wheel__inner__list__item">
+          <img src=${figures[el]}></img>
+        </li>`;
+      WHEEL.insertAdjacentHTML("beforeEnd", template);
+    });
+  }
 
   document.querySelector(".wheel_area__wheel__inner").appendChild(fragment);
 }
@@ -59,10 +77,6 @@ function fillTableContent(figures) {
   document.querySelector(".table").appendChild(fragment);
 }
 
-function viewRandomFigures() {
-  count = randomInteger(0, 39);
-}
-
 function randomInteger(min, max) {
   let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
@@ -72,13 +86,13 @@ function randomInteger(min, max) {
 /////////////////////////////////////////////////////////////////
 
 function startWheel() {
-  if (ifFirstPress) {
+  if (ifStartPress) {
     tl.to(".wheel_area__wheel__inner__list__item", {
       y: -7040,
       duration: 0.3,
     });
+    ifStartPress = false;
   }
-  ifFirstPress = false;
-  tl.duration(0.3);
-  tl.resume();
+
+  tl.play({ duration: 0.3 });
 }
