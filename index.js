a41848a03c050d86aae2b94672a06900616aff47
DIMA <t0perxxl@gmail.com>
2021-10-18 17:19:23 +0300

    try to use progress() for  control then stop

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { figures } from "./modules/storage.js";
-gsap.registerPlugin(ScrollTrigger);
-var tl = gsap.timeline({ repeat: -1, delay: 0 });
+// gsap.registerPlugin(ScrollTrigger);
 
 const WHEEL = document.querySelector(".wheel_area__wheel__inner__list");
 const TABLE = document.querySelector(".table__figures");
 
-let ifStartPress = true;
+// let ifStartPress = true;
 let count = 0;
+let startPressed = true;
 
 fillWheelContent(figures);
 fillTableContent(figures);
@@ -14,66 +14,31 @@ fillTableContent(figures);
 document
   .querySelector(".wheel_area__buttons_wrap__start")
   .addEventListener("click", () => {
-    ifStartPress = true;
-    fillWheelContent(figures);
-    startWheel();
+    shiftWheel();
+    startPressed = false;
   });
 document
   .querySelector(".wheel_area__buttons_wrap__stop")
   .addEventListener("click", () => {
-    tl.duration(7);
-    setTimeout(() => tl.duration(10), 1000);
-    setTimeout(() => tl.pause(), 2500);
-    setTimeout(() => fillWheelContent(figures), 2500);
+    shiftWheel();
+    startPressed = true;
   });
 
 function fillWheelContent(figures) {
-  console.log(ifStartPress);
+  let figures42 = [...figures, figures[0], figures[1]];
   let fragment = document.createDocumentFragment();
   WHEEL.innerHTML = "";
   fragment.appendChild(WHEEL);
 
-  if (ifStartPress) {
-    figures.forEach((el, index) => {
-      let template = ` <li id = "item${index}" class="wheel_area__wheel__inner__list__item">
+  figures42.forEach((el, index) => {
+    let template = ` <li 
+     class="wheel_area__wheel__inner__list__item item${index + 1}">
+
+
           <img src=${el}></img>
         </li>`;
-      WHEEL.insertAdjacentHTML("beforeEnd", template);
-    });
-  }
-  if (!ifStartPress) {
-    count = randomInteger(0, 39);
-    let maxThreeArr = [count, count + 1, count + 2];
-
-    if (count === figures.length - 1) {
-      maxThreeArr = [count, 0, 1];
-    }
-    if (count === figures.length - 2) {
-      maxThreeArr = [count, count + 1, 0];
-    }
-
-    maxThreeArr.forEach((el, index) => {
-      let template = ` <li id = "item${index}" class="wheel_area__wheel__inner__list__item">
-          <img src=${figures[el]}></img>
-        </li>`;
-      WHEEL.insertAdjacentHTML("beforeEnd", template);
-    });
-    setTimeout(
-      () =>
-        gsap.fromTo(
-          ".wheel_area__wheel__inner__list__item",
-          {
-            y: 80,
-          },
-          {
-            y: 0,
-            ease: "bounce",
-            duration: 2,
-          }
-        ),
-      100
-    );
-  }
+    WHEEL.insertAdjacentHTML("beforeEnd", template);
+  });
 
   document.querySelector(".wheel_area__wheel__inner").appendChild(fragment);
 }
@@ -85,11 +50,48 @@ function fillTableContent(figures) {
   figures.forEach(function (figure, index) {
     let template = `  
            <li id = ${index}>
-      <img src=${figure} id=${index} alt=""></img>
+      <img src=${figure} alt=""></img>
     </li>`;
     TABLE.insertAdjacentHTML("beforeEnd", template);
   });
   document.querySelector(".table").appendChild(fragment);
+}
+
+function fillWheelWithRandomThreeFigures() {
+  let fragment = document.createDocumentFragment();
+  WHEEL.innerHTML = "";
+  fragment.appendChild(WHEEL);
+
+  count = randomInteger(0, 39);
+  let maxThreeArr = [count, count + 1, count + 2];
+  if (count === figures.length - 1) {
+    maxThreeArr = [count, 0, 1];
+  }
+  if (count === figures.length - 2) {
+    maxThreeArr = [count, count + 1, 0];
+  }
+  maxThreeArr.forEach((el) => {
+    let template = ` <li class='wheel_area__wheel__inner__list__item'>
+          <img src=${figures[el]}></img>
+        </li>`;
+    WHEEL.insertAdjacentHTML("beforeEnd", template);
+  });
+  setTimeout(
+    () =>
+      gsap.fromTo(
+        ".wheel_area__wheel__inner__list__item",
+        {
+          y: 90,
+        },
+        {
+          y: 0,
+          ease: "bounce",
+          duration: 2,
+        }
+      ),
+    100
+  );
+  document.querySelector(".wheel_area__wheel__inner").appendChild(fragment);
 }
 
 function randomInteger(min, max) {
@@ -97,17 +99,47 @@ function randomInteger(min, max) {
   return Math.floor(rand);
 }
 
-/////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
+// Таким образом, используя метод getLabelTime (), вы можете получить это значение в переменной
+// а с помощью метода() вы получаете общее время на временной шкале.
 
-function startWheel() {
-  if (ifStartPress) {
-    tl.to(".wheel_area__wheel__inner__list__item", {
-      y: -7040,
-      duration: 0.3,
-    });
-    ifStartPress = false;
+// const= 2;
+const workListHeight = -160;
+
+const tl = gsap
+  .timeline({ repeat: -1, paused: true })
+  .to(".wheel_area__wheel__inner__list", {
+    y: -7420,
+    duration: 200,
+    ease: "none",
+  });
+
+function shiftWheel() {
+  // console.log(tl.progress());
+  // one.pause();
+  if (startPressed) {
+    // tl.progress(0).then(tl.pause());
+    tl.play();
+    // tl0.3);
   }
+  if (!startPressed) {
+    console.log("stop");
+    // console.log(tl.labels);
 
-  tl.play({ duration: 0.3 });
+    // console.log(figure1.progress());
+
+    // gsap.to(tl, { progress: tl.labels["figure30"], ease: "none" });
+    tl.progress(0.50005).pause();
+    // tl.pause();
+
+    // tl.pause();
+    console.log(tl.totalProgress());
+  }
+  /////////////////////////////////////////////////////////////////////
 }
+
+function generateSteps(count) {
+  count = 0.013685;
+  // console.log(count + count + count);
+  console.log(0.05308);
+}
+generateSteps();
---
+++ usless.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const listHeight = document.querySelector(
+  ".wheel_area__wheel__inner__list"
+).scrollHeight;
+const workItemWindow = document.querySelector(
+  ".wheel_area__wheel__inner"
+).offsetHeight;
+const workListHeight = listHeight - workItemWindow;
+console.log(workListHeight);
+/////////////////////////////////////////////////////////////////////////////////////////////
+// сколько времени длительность(DURATION) столько и задежку(delay) элементам чтоб одновременно запускалась
+//анимация каждой фигуры
+
+//////////////////////////////FUNCTIONS/////////////////////////////////////////////////////////////
+////////////////////////////////////////      1    /////////////////////////////////////////////////
+function createTl() {
+  let fragment = document.createDocumentFragment();
+  const allItems = document.createElement("span");
+  fragment.appendChild(allItems);
+
+  for (let index = 1; index <= 40; index++) {
+    let template = `.to(".item${index}" , { y: ${workListHeight}, duration: DURATION }, 
+      "figure${index}")`;
+    allItems.insertAdjacentHTML("beforeEnd", template);
+  }
+  console.log(fragment.textContent);
+}
+createTl();
+////////////////////////////////////////    END of 1    ///////////////////////////////////////////////
+////////////////////////////////////////       2        /////////////////////////////////////////////////
+function createTl() {
+  let fragment = document.createDocumentFragment();
+  const allItems = document.createElement("span");
+  fragment.appendChild(allItems);
+
+  for (let index = 1; index <= 40; index++) {
+    let template = `.fromTo(".item${index}",{y: 0} , {  y: -7420,
+    duration: 200,
+    ease: "none",}).addLabel(
+      "figure${index}")`;
+    allItems.insertAdjacentHTML("beforeEnd", template);
+  }
+  console.log(fragment.textContent);
+}
+createTl();
